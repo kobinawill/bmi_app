af5e536515e23b34f4fc3667f33cfcbc2ee195cd
kobinawill <kobinawill@gmail.com>
2020-04-01 19:01:04 +0000

    Applicaiton done..

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -9,6 +9,37 @@ class HomePage extends StatefulWidget{
 }
 
 class home_page extends State<HomePage>{
+  final TextEditingController heightFigure = new TextEditingController();
+  final TextEditingController weightFigure = new TextEditingController();
+  double finalResult;
+  String finalText = "Please enter values";
+  String analysis = "(enter values to continue)";
+  var Category = ["Overweight", "Normal", "Underweight"];
+
+  void calculateBmi (){
+    setState(() {
+      if ((heightFigure.text.isNotEmpty && int.parse(heightFigure.text) > 0) && (weightFigure.text.isNotEmpty && int.parse(weightFigure.text) > 0))
+      {
+        finalResult = 703 * int.parse(weightFigure.text) / (int.parse(heightFigure.text) * int.parse(heightFigure.text));
+        finalText = "Your BMI: ${finalResult.toStringAsFixed(2)}";
+
+        if (finalResult <= 10){
+          analysis = Category[0];
+        }
+        else if(finalResult > 10 && finalResult <= 20){
+          analysis = Category[1];
+        }
+        else {
+          analysis = Category[2];
+        }
+      }
+      else{
+        finalText = "Please enter values";
+        analysis = "(enter values to continue)";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
         return new Scaffold(
@@ -44,7 +75,7 @@ class home_page extends State<HomePage>{
                            keyboardType: TextInputType.number,
                          ),
                          new TextField(
-                           controller: null,
+                           controller: heightFigure,
                            decoration: new InputDecoration(
                              labelText: "Height in feet",
                              icon: new Icon(Icons.insert_chart)
@@ -52,7 +83,7 @@ class home_page extends State<HomePage>{
                            keyboardType: TextInputType.number,
                          ),
                          new TextField(
-                           controller: null,
+                           controller: weightFigure,
                            decoration: new InputDecoration(
                              labelText: "Weight in lb",
                              icon: new Icon(Icons.line_weight)
@@ -60,7 +91,7 @@ class home_page extends State<HomePage>{
                            keyboardType: TextInputType.number,
                          ),
                          new Padding(padding: new EdgeInsets.all(7.0)),
-                         new RaisedButton(onPressed: null,
+                         new RaisedButton(onPressed: calculateBmi,
                          child: new Text("Calculate",
                          style: new TextStyle(color: Colors.white),),
                          color: Colors.black38,)
@@ -69,18 +100,19 @@ class home_page extends State<HomePage>{
                    ),
                    new Container(
                      alignment: Alignment.center,
-                     child: new Text("Your BMI: ",
+                     child: new Text(finalText,
                         style: new TextStyle(
                           color: Colors.blue,
                           fontStyle: FontStyle.italic,
-                          fontSize: 20
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500
                         ),
                      ),
                    ),
                    new Padding(padding: new EdgeInsets.all(7)),
                    new Container(
                      alignment: Alignment.center,
-                     child: new Text("YourAnalysis",
+                     child: new Text(analysis,
                      style: new TextStyle(color: Colors.black38,
                      fontSize: 18,
                      fontWeight: FontWeight.w900),),
